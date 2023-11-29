838ae7ee4aa1633895953d0d260233a39eeb3eda
Roggero Spezialy <130925537+RoggeroSpez@users.noreply.github.com>
2023-11-29 12:29:43 -0800

    Update home.js

--- src/Pages/home.js
+++ src/Pages/home.js
@@ -1,10 +1,10 @@
-import Headshot from '/Media/Headshot';
+import HeadShot from "./Media/headshot";
 
 const Home = () => {
     return (<>
     <section className="Home">
         <section className="introdcution">
-            <Headshot/>
+            <HeadShot Headshot = {HeadShot}/>
             <h3>Hello and Welcome to my portfolio!</h3>
             <p>My name is <span>Roggero Spezialy</span> and this portfolio is made to help showcase the skills as well as the abilities I have learned while going through a coding bootcamp via SprinBoard or through self research on various platforms.</p>
             <p>While I did go through the Front-End Software Development course, my end goal is to create apps and video games. I love the idea of creating something that helps people in various of ways and a good way is through video games.</p>
