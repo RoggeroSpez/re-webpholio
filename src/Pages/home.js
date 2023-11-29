216295e93d2b6ad8082dcc76462394a0763c0430
Roggero Spezialy <130925537+RoggeroSpez@users.noreply.github.com>
2023-11-29 13:52:53 -0800

    Update home.js

--- src/Pages/home.js
+++ src/Pages/home.js
@@ -11,7 +11,7 @@ const Home = () => {
         <section className="introdcution">
             <img className="HeadShot" src={HeadShot} alt="HeadShot"/>
             <h3>Hello and Welcome to my portfolio!</h3>
-            <p>My name is <span>Roggero Spezialy</span> and this portfolio is made to help showcase the skills as well as the abilities I have learned while going through a coding bootcamp via SprinBoard or through self research on various platforms.</p>
+            <p>My name is Roggero Spezialy and this portfolio is made to help showcase the skills as well as the abilities I have learned while going through a coding bootcamp via SprinBoard or through self research on various platforms.</p>
             <p>While I did go through the Front-End Software Development course, my end goal is to create apps and video games. I love the idea of creating something that helps people in various of ways and a good way is through video games.</p>
         </section>
     </section>
