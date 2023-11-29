d0d76a1a587e9b331825928f446d92be494ad3ff
Roggero Spezialy <130925537+RoggeroSpez@users.noreply.github.com>
2023-11-29 14:28:50 -0800

    Updates

--- src/Pages/projects.js
+++ src/Pages/projects.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import './card.css';
 const Projects = () => {
     return(
         <div>
--- src/Pages/refrences.js
+++ src/Pages/refrences.js
@@ -1,4 +1,6 @@
 import React from "react";
+import "./card.css";
+
 const Refrences = () => {
     return(
         <div>
