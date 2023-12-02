06af9e68894fe16f9c17d14ae47d23c64cc7bc99
Roggero Spezialy <130925537+RoggeroSpez@users.noreply.github.com>
2023-12-01 16:53:41 -0800

    Update experience.js

--- src/Pages/experience.js
+++ src/Pages/experience.js
@@ -8,12 +8,12 @@ return (
 <tbody>
 <tr><td><h2>School</h2></td></tr>
 <tr><td>SpringBoard (Software Development Front-end)</td> <td>02/2023-11/2023</td> <td> Basic coding languages (html, css, javascript, react)</td></tr>
-<tr></tr>
+<tr><td></td></tr>
 <tr><td> Fred G. Acosta Job-corps (Vocational School)</td> <td> 05/2009-05/2011</td> <td> Facilities Maintenance General Education Diploma -09/200 Vocational Training -05/2011</td></tr>
-<tr></tr>
+<tr><td></td></tr>
 <tr><td><h2>Work</h2></td></tr>
 <tr><td>Amazon Style</td> <td>11/2021-11/2023 839 Americana Way, Glendale, CA</td> <td>While working at Amazon Style, my tasks include gathering requested items for the customer and delivering them to the required place, another task is to organize the products as well as help keep inventory. I am also trained in multiple parts of the store's operational areas such as Problem Solving.</td></tr>
-<tr></tr>
+<tr><td></td></tr>
 <tr><td> Starbucks</td> <td>05/2016-11/2022 114 N. Brand Blvd Glendale, CA</td> <td>While working at Starbucks, my tasks included; Greeting patrons, able to meet specifications requested by patrons,up-selling new products, cleaning various areas around the location, preparing various ingredients, handling financial transactions (Cash, Credit Card, Gift cards), and training future partners.</td></tr>
 </tbody>
 </table>
