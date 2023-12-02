2767cad58d4cde64c66efabf442f2024b3fb069a
Roggero Spezialy <130925537+RoggeroSpez@users.noreply.github.com>
2023-12-01 16:24:24 -0800

    Update experience.js

--- src/Pages/experience.js
+++ src/Pages/experience.js
@@ -16,13 +16,13 @@ Fred G. Acosta Job-corps (Vocational School)
             <p>
 Amazon Style
 11/2021-11/2023
-839 Americana Way, Glendale, CA
+839 Americana Way, Glendale, CA             
 While working at Amazon Style, my tasks include gathering requested items for the customer and delivering them to the required place, another task is to organize the products as well as help keep inventory. I am also trained in multiple parts of the store's operational areas such as Problem Solving.
 </p>
 <p>
 Starbucks
 05/2016-11/2022
-114 N. Brand Blvd Glendale, CA
+114 N. Brand Blvd Glendale, CA          
 While working at Starbucks, my tasks included; Greeting patrons, able to meet specifications requested by patrons,up-selling new products, cleaning various areas around the location, preparing various ingredients, handling financial transactions (Cash, Credit Card, Gift cards), and training future partners.
             </p>
         </div>
