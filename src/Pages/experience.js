dcd4e21e9d1b8383e5f8c0a6d0d0763683568b1d
Roggero Spezialy <130925537+RoggeroSpez@users.noreply.github.com>
2023-12-01 16:03:36 -0800

    Update experience.js

--- src/Pages/experience.js
+++ src/Pages/experience.js
@@ -4,15 +4,19 @@ const Exerience = () => {
     return (
         <div>
             <h1>Experience</h1>
-            <h2>School</h2>
-            <p>
+            <table>
+            <tr><td><h2>School</h2></td></tr>
+            <tr>
+                <td><p>
 SpringBoard (Software Development Front-end)
 02/2023-11/2023 Basic coding languages (html, css, javascript, react)
+
 Fred G. Acosta Job-corps (Vocational School)
 05/2009-05/2011 Facilities Maintenance General Education Diploma -09/200 Vocational Training -05/2011
-            </p>
-            <h2>work</h2>
-            <p>
+            </p></td>
+            </tr>
+            <tr><td><h2>work</h2></td></tr>
+            <td><p>
 Amazon Style
 11/2021-11/2023
 839 Americana Way, Glendale, CA
@@ -21,7 +25,8 @@ Starbucks
 05/2016-11/2022
 114 N. Brand Blvd Glendale, CA
 While working at Starbucks, my tasks included; Greeting patrons, able to meet specifications requested by patrons,up-selling new products, cleaning various areas around the location, preparing various ingredients, handling financial transactions (Cash, Credit Card, Gift cards), and training future partners.
-            </p>
+            </p></td>
+            </table>
         </div>
     )
 }
